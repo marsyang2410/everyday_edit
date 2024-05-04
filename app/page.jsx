5e3c672b04b321402995d76555de6f59e7fe0803
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2024-05-04 12:07:58 +0800

    addjust opencv

--- app/page.jsx
+++ app/page.jsx
@@ -1,137 +1,133 @@
-'use client'
+"use client";
 
 import SlideBar from "./components/SlideBar.client.jsx";
 import ImageUploader from "./components/ImageUpload.jsx";
-import React , { useState , useEffect , useRef } from "react";
-import cv from "opencv.js"
-import ShaderObject from "./components/shader.js";
-
+import React, { useState, useEffect, useRef } from "react";
+// const cv = require("./opencv.js");
 export default function Home() {
-  const [src, setSrc] = useState(null); 
+  const [src, setSrc] = useState(null);
   const canvasRef = useRef(null);
-  const histCanvas = useRef(null)
-  const [brightness, setbrightness] = useState(1)
-  const [contrast, setcontrast] = useState(127)
-  const [saturation, setsaturation] = useState(127)
-  const [whitePatchness, setwhitepatch] = useState(127)
-
+  const histCanvas = useRef(null);
+  const [brightness, setbrightness] = useState(1);
+  const [contrast, setcontrast] = useState(127);
+  const [saturation, setsaturation] = useState(127);
+  const [whitePatchness, setwhitepatch] = useState(127);
 
   const matRef = useRef(null); // Using a ref to manage OpenCV Mat objects
 
   const drawDefaultBackground = () => {
     const canvas = histCanvas.current;
     if (!canvas) return;
-    
-    const ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'lightgray'; // Set background color
+
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "lightgray"; // Set background color
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
-    ctx.fillStyle = 'black'; // Set text color
-    ctx.font = '20px Arial';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
+
+    ctx.fillStyle = "black"; // Set text color
+    ctx.font = "20px Arial";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
     // ctx.fillText("No Image Loaded", canvas.width / 2, canvas.height / 2);
   };
-
 
   useEffect(() => {
     if (!src) {
       drawDefaultBackground();
-    }    
+    }
     // Clean up the previous Mat before creating a new one
     if (matRef.current) {
       matRef.current.delete();
     }
     if (src && canvasRef.current) {
       const canvas = canvasRef.current;
-      const ctx = canvas.getContext('2d');
+      const ctx = canvas.getContext("2d");
       const img = new Image();
       img.onload = () => {
-          // Resize canvas to match image dimensions
-          canvas.width = img.width;
-          canvas.height = img.height;
-          ctx.drawImage(img, 0, 0); // Draw the image at the top left corner
-          matRef.current = cv.imread(canvasRef.current);
-          drawHistGram()
+        // Resize canvas to match image dimensions
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0); // Draw the image at the top left corner
+        matRef.current = cv.imread(canvasRef.current);
+        drawHistGram();
       };
       img.src = src;
     }
   }, [src]); // Dependency only on src
-  useEffect(()=>{
-    console.log("matrix change")
-    drawHistGram()
-  },[matRef.current])
-  
+  useEffect(() => {
+    console.log("matrix change");
+    drawHistGram();
+  }, [matRef.current]);
+
   const drawHistGram = () => {
     if (cv && cv.imread) {
-        if(!canvasRef.current) return
-        let srcMat = new cv.Mat(); // Load your image into srcMat
-        matRef.current.copyTo(srcMat);
-        cv.cvtColor(srcMat, srcMat, cv.COLOR_RGBA2GRAY, 0); // Convert to grayscale
+      if (!canvasRef.current) return;
+      let srcMat = new cv.Mat(); // Load your image into srcMat
+      matRef.current.copyTo(srcMat);
+      cv.cvtColor(srcMat, srcMat, cv.COLOR_RGBA2GRAY, 0); // Convert to grayscale
 
-        // Calculate histogram
-        let hist = new cv.Mat();
-        let mask = new cv.Mat();
-        let histSize = [256];
-        let ranges = [0, 255];
-        let srcVec = new cv.MatVector();
-        srcVec.push_back(srcMat);
-        cv.calcHist(srcVec, [0], mask, hist, histSize, ranges, false);
+      // Calculate histogram
+      let hist = new cv.Mat();
+      let mask = new cv.Mat();
+      let histSize = [256];
+      let ranges = [0, 255];
+      let srcVec = new cv.MatVector();
+      srcVec.push_back(srcMat);
+      cv.calcHist(srcVec, [0], mask, hist, histSize, ranges, false);
 
-        let result = cv.minMaxLoc(hist, mask);
-        let max = result.maxVal;
+      let result = cv.minMaxLoc(hist, mask);
+      let max = result.maxVal;
 
-        // Setup canvas dimensions and drawing scale
-        let canvas = histCanvas.current;
-        const ctx = histCanvas.current.getContext('2d', { willReadFrequently: true });
-        let width = canvas.width;
-        let height = canvas.height;
-        let binWidth = Math.round(width / histSize[0]); // Width of each bin
+      // Setup canvas dimensions and drawing scale
+      let canvas = histCanvas.current;
+      const ctx = histCanvas.current.getContext("2d", {
+        willReadFrequently: true,
+      });
+      let width = canvas.width;
+      let height = canvas.height;
+      let binWidth = Math.round(width / histSize[0]); // Width of each bin
 
-        // Clear the canvas
-        ctx.fillStyle = 'lightgray'; // Set background color
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = 'white';
+      // Clear the canvas
+      ctx.fillStyle = "lightgray"; // Set background color
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.fillStyle = "white";
 
-        // Draw histogram
-        for (let i = 0; i < histSize[0]; i++) {
-            let binVal = hist.data32F[i] * height / max;
-            ctx.fillRect(i * binWidth, height - binVal, binWidth, binVal);
-        }
+      // Draw histogram
+      for (let i = 0; i < histSize[0]; i++) {
+        let binVal = (hist.data32F[i] * height) / max;
+        ctx.fillRect(i * binWidth, height - binVal, binWidth, binVal);
+      }
 
-        // Clean up
-        srcMat.delete();
-        hist.delete();
-        mask.delete();
-        srcVec.delete();
+      // Clean up
+      srcMat.delete();
+      hist.delete();
+      mask.delete();
+      srcVec.delete();
     } else {
-        console.error('OpenCV.js not loaded');
+      console.error("OpenCV.js not loaded");
     }
-}
-
+  };
 
   // Example function to adjust brightness using a slider
   const handleBrightness = (newBrightness) => {
     if (!matRef.current) return;
     if (canvasRef.current) {
       let dstMat = new cv.Mat();
-      matRef.current.convertTo(dstMat, -1, 1, newBrightness - brightness)
+      matRef.current.convertTo(dstMat, -1, 1, newBrightness - brightness);
       console.log("Canvas reference:", canvasRef.current);
-      console.log("dst matrix:", dstMat)
+      console.log("dst matrix:", dstMat);
       if (dstMat.empty()) {
         console.error("Destination matrix is empty.");
         return;
       }
       try {
-          cv.imshow(canvasRef.current, dstMat);
-          matRef.current.delete();
-          matRef.current = dstMat;
+        cv.imshow(canvasRef.current, dstMat);
+        matRef.current.delete();
+        matRef.current = dstMat;
       } catch (error) {
-          console.error("Failed to display image on canvas:", error);
+        console.error("Failed to display image on canvas:", error);
       }
-      setbrightness(newBrightness)
+      setbrightness(newBrightness);
     }
-
   };
   function handleContrast(contrastValue) {
     if (!matRef.current) return;
@@ -141,10 +137,15 @@ export default function Home() {
 
     const B = 0 / 255.0;
     const c = contrastValue / 255.0;
-    const k = Math.tan((45 + 44 * c) * Math.PI / 180);
-    console.log(k)
+    const k = Math.tan(((45 + 44 * c) * Math.PI) / 180);
+    console.log(k);
 
-    matRef.current.convertTo(dst, cv.CV_8U, k, 127.5 * (1 - B) * (-k) + 127.5 * (1 + B));
+    matRef.current.convertTo(
+      dst,
+      cv.CV_8U,
+      k,
+      127.5 * (1 - B) * -k + 127.5 * (1 + B)
+    );
     try {
       cv.imshow(canvasRef.current, dst);
       matRef.current.delete();
@@ -169,10 +170,15 @@ export default function Home() {
     let saturation = hsvChannels.get(1);
 
     // Multiply the saturation channel by the factor
-    let saturationFactor = new cv.Mat(saturation.rows, saturation.cols, saturation.type(), new cv.Scalar(saturationValue));
+    let saturationFactor = new cv.Mat(
+      saturation.rows,
+      saturation.cols,
+      saturation.type(),
+      new cv.Scalar(saturationValue)
+    );
     let newSaturation = new cv.Mat();
     cv.multiply(saturation, saturationFactor, newSaturation);
-    saturation.delete();  // Clean up old saturation
+    saturation.delete(); // Clean up old saturation
 
     // Replace the channel and merge back
     hsvChannels.set(1, newSaturation);
@@ -184,11 +190,11 @@ export default function Home() {
 
     // Display the result
     try {
-        cv.imshow(canvas, bgrMat);
-        matRef.current.delete();
-        matRef.current = bgrMat;
+      cv.imshow(canvas, bgrMat);
+      matRef.current.delete();
+      matRef.current = bgrMat;
     } catch (error) {
-        console.error("Failed to display image on canvas:", error);
+      console.error("Failed to display image on canvas:", error);
     }
 
     // Clean up
@@ -199,20 +205,27 @@ export default function Home() {
     setsaturation(saturationValue);
   }
 
-  function handleWhitePatch(whitePatchValue){
+  function handleWhitePatch(whitePatchValue) {
     if (!canvasRef.current) return;
 
     let gray = new cv.Mat();
     cv.cvtColor(matRef.current, gray, cv.COLOR_BGR2GRAY);
 
     let sorted = cv.sort(gray, cv.SORT_ASCENDING);
-    let index = Math.floor(sorted.rows * sorted.cols * whitePatchValue / 100);
-    let threshold = sorted.ucharPtr(Math.floor(index / sorted.cols), index % sorted.cols)[0];
+    let index = Math.floor((sorted.rows * sorted.cols * whitePatchValue) / 100);
+    let threshold = sorted.ucharPtr(
+      Math.floor(index / sorted.cols),
+      index % sorted.cols
+    )[0];
     sorted.delete();
 
     let whitePatch = new cv.Mat();
     gray.convertTo(whitePatch, cv.CV_32F, 1.0 / threshold);
-    cv.min(whitePatch, new cv.Mat(gray.rows, gray.cols, cv.CV_32F, new cv.Scalar(1.0)), whitePatch);
+    cv.min(
+      whitePatch,
+      new cv.Mat(gray.rows, gray.cols, cv.CV_32F, new cv.Scalar(1.0)),
+      whitePatch
+    );
 
     let output = new cv.Mat();
     whitePatch.convertTo(whitePatch, cv.CV_8U, 255.0);
@@ -223,7 +236,7 @@ export default function Home() {
       matRef.current.delete();
       matRef.current = output;
     } catch (error) {
-        console.error("Failed to display image on canvas:", error);
+      console.error("Failed to display image on canvas:", error);
     }
     gray.delete();
     whitePatch.delete();
@@ -232,43 +245,47 @@ export default function Home() {
 
   const sliders = [
     {
-        min: -50,
-        max: 50,
-        step: 5,
-        title: "亮度",
-        initialData: 0,
-        onValueChange: handleBrightness
+      min: -50,
+      max: 50,
+      step: 5,
+      title: "亮度",
+      initialData: 0,
+      onValueChange: handleBrightness,
     },
     {
-        min: -125,
-        max: 125,
-        step: 5,
-        title: "對比",
-        initialData: 0,
-        onValueChange: handleContrast
+      min: -125,
+      max: 125,
+      step: 5,
+      title: "對比",
+      initialData: 0,
+      onValueChange: handleContrast,
     },
     {
-        min: 0,
-        max: 255,
-        step: 1,
-        title: "鮮豔度",
-        initialData: 127,
-        onValueChange: handleColorSaturation
+      min: 0,
+      max: 255,
+      step: 1,
+      title: "鮮豔度",
+      initialData: 127,
+      onValueChange: handleColorSaturation,
     },
     {
-        min: 0,
-        max: 100,
-        step: 1,
-        title: "白平衡",
-        initialData: 50,
-        onValueChange: handleWhitePatch
-    }
+      min: 0,
+      max: 100,
+      step: 1,
+      title: "白平衡",
+      initialData: 50,
+      onValueChange: handleWhitePatch,
+    },
   ];
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-row w-full h-screen">
         <div className="w-1/2 h-400 p-4">
-            <ImageUploader imageSrc={src} setImageSrc={setSrc} canvasRef={canvasRef}/>
+          <ImageUploader
+            imageSrc={src}
+            setImageSrc={setSrc}
+            canvasRef={canvasRef}
+          />
         </div>
         <div className="flex-1 p-4 max-w-md">
           <div className="flex flex-col h-full">
@@ -291,13 +308,18 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <script
+        async
+        src="https://docs.opencv.org/4.5.4/opencv.js"
+        type="text/javascript"
+      ></script>
     </main>
   );
 }
 
 export async function loader({ params, request, context }) {
   const initialData = {
-    message: 'This is server-side fetched data.'
+    message: "This is server-side fetched data.",
   };
 
   return initialData;
