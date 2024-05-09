c8f03fc3273aa3ec6ee755c0092954b86f3cefe5
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2024-05-09 10:43:16 +0800

    update

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,11 @@ import SlideBar from "./components/SlideBar.client.jsx";
 import ImageUploader from "./components/ImageUpload.jsx";
 import Header from "./components/Header.jsx"
 import React, { useState, useEffect, useRef } from "react";
+import {
+  Collapse,
+  Ripple,
+  initTWE,
+} from "tw-elements";
 import * as cv from "@techstark/opencv-js"
 
 const DEBUG = 0;
@@ -322,19 +327,38 @@ export default function Home() {
       </div>
       <div className="flex-1 p-4">
         <div className="flex flex-col h-full"> {/* Adjusted to fill the column space */}
-          <div className="overflow-auto flex-grow">
-            {sliders.map((slider, index) => (
-              <SlideBar
-                key={index}
-                min={slider.min}
-                max={slider.max}
-                step={slider.step}
-                initialData={slider.initialData}
-                title={slider.title}
-                onValueChange={slider.onValueChange}
-              />
-            ))}
-          </div>
+        <button
+          className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+          type="button"
+          data-twe-collapse-init
+          data-twe-ripple-init
+          data-twe-ripple-color="light"
+          data-twe-target="#collapseExample"
+          aria-expanded="false"
+          aria-controls="collapseExample"
+        >
+          Button with data-twe-target
+        </button>
+
+        <div
+          className="!visible hidden"
+          id="collapseExample"
+          data-twe-collapse-item
+        >
+        <div className="overflow-auto flex-grow">
+          {sliders.map((slider, index) => (
+            <SlideBar
+              key={index}
+              min={slider.min}
+              max={slider.max}
+              step={slider.step}
+              initialData={slider.initialData}
+              title={slider.title}
+              onValueChange={slider.onValueChange}
+            />
+          ))}
+        </div>
+        </div>
           <div className="mt-4">
             <canvas ref={histCanvas} className="w-full"></canvas>
           </div>
